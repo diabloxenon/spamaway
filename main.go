bc50d3b34f4338c04fbc15333d529f3dc04dbb3a
Naman Bishnoi <researchlabs@ncrypto.co>
2020-05-04 23:00:31 +0530

    new dataset added for testing, performance benchmarking added
    Signed-off-by: Naman Bishnoi <researchlabs@ncrypto.co>

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ import (
 	"strings"
 )
 
-type WordDict map[string]bool
-type FeatMat [][]int
-type LabelMat []int
-
 // BuildDictionary creates dictionary from all the emails in directory
 func BuildDictionary(dir string) ([]utils.WordDict, error) {
 	var err error
@@ -94,7 +90,7 @@ func BuildDictionary(dir string) ([]utils.WordDict, error) {
 }
 
 // BuildFeatures returns the feature matrix
-func BuildFeatures(dir string, dictionary utils.WordDict) (FeatMat, error) {
+func BuildFeatures(dir string, dictionary utils.WordDict) (utils.FeatMat, error) {
 	// Read the file names
 	emailList, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -129,7 +125,7 @@ func BuildFeatures(dir string, dictionary utils.WordDict) (FeatMat, error) {
 	return featMatrix, nil
 }
 
-func BuildLabels(dir string) (LabelMat, error){
+func BuildLabels(dir string) (utils.LabelMat, error){
 	// Read the file names
 	emailList, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -150,11 +146,83 @@ func BuildLabels(dir string) (LabelMat, error){
 	return labelMat, nil
 }
 
+func ReadEmail(emailName string) ([]string, error){
+	wordlist := []string{}
+	data, err := ioutil.ReadFile(emailName)
+		if err != nil {
+			return nil, fmt.Errorf("File opening failed %s", err)
+		}
+		// Breaks the email into lines.
+		dat := strings.Split(string(data), "\n")
+		for i, line := range dat{
+			// Body of email is only 3rd line of text file
+			if i == 2{
+				words := strings.Split(line, " ")
+				wordlist = append(wordlist, words...)
+			}
+		}
+	// Remove Duplicates
+	wordDict := utils.Set(wordlist)
+	
+	// Useless chars deleted
+	for word := range wordDict{
+		if len(word) == 1 || !utils.IsAlpha(word) {
+			delete(wordDict, word)
+		}
+	}
+
+	return utils.MapToArr(wordDict), nil
+}
+
+func BuildFmtDict(filename string) ([][]string, error){
+	goodwordlist := []string{}
+	spamwordlist := []string{}
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("File opening failed %s", err)
+	}
+			// Breaks the email into lines.
+			dat := strings.Split(string(data), "\n")
+			//Last line is blank
+			for _, line := range dat[0:len(dat)-1]{
+				// Remove labels from start
+				words := strings.Split(line[2:len(line)], " ")
+				switch line[0] {
+				case 49:
+					// Label of email is 1 i.e. (49 in ascii) then it is a spam mail
+					spamwordlist = append(spamwordlist, words...)
+				case 48:
+					// Label of email is 0 i.e. (48 in ascii) then it is a fam mail
+					goodwordlist = append(goodwordlist, words...)
+				}
+			}
+	return [][]string{goodwordlist, spamwordlist}, nil
+}
+
+func ReadFmtDict(filename string) ([]string, error){
+	wordlist := []string{}
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("File opening failed %s", err)
+	}
+			// Breaks the email into lines.
+			dat := strings.Split(string(data), "\n")
+			//Last line is blank
+			for _, line := range dat[0:len(dat)-1]{
+				// Remove labels from start
+				words := strings.Split(line[2:len(line)], " ")
+				wordlist = append(wordlist, words...)
+			}
+	return wordlist, nil
+}
+
 func main() {
-	trainDir := "dataset/train_data"
+	// trainDir := "dataset/train_data"
 	
 	fmt.Println("1. Building dictionary")
-	dict, err := BuildDictionary(trainDir)
+	// dict, err := BuildDictionary(trainDir)
+	// utils.Check(err)
+	dict, err := BuildFmtDict("dataset/spam_train.txt")
 	utils.Check(err)
 
 	// fmt.Println("2. Building training features and labels")
@@ -168,20 +236,89 @@ func main() {
 	// utils.Check(err)
 
 	var (
-		Fam bayesian.Class = "Fam"   // The good ones
-		Spam bayesian.Class = "Spam" // The bad ones
+		Fam bayesian.Class = "fam"   // The good ones
+		Spam bayesian.Class = "spam" // The bad ones
 	)
-	classifier := bayesian.NewClassifierTfIdf(Spam, Fam)
+	// New Multinomial TF classifier
+	classifier := bayesian.NewClassifier(bayesian.MultinomialTf)
 
-	famMails := utils.MapToArr(dict[0])
-	spamMails := utils.MapToArr(dict[1])
+	// famMails := utils.MapToArr(dict[0])
+	// spamMails := utils.MapToArr(dict[1])
 
-	classifier.Learn(famMails, Fam)
-	classifier.Learn(spamMails, Spam)
+	famMails := &dict[0]
+	spamMails := &dict[1]
 
-	classifier.ConvertTermsFreqToTfIdf()
+	// classifier.Learn(famMails, Fam)
+	// classifier.Learn(spamMails, Spam)
 
-	// scores, likely, _ := classifier.LogScores(
-	// 	[]string{}
-	// )
+	// Do learning using two documents
+	classifier.Learn(
+		bayesian.NewDocument(Fam, *famMails),
+		bayesian.NewDocument(Spam, *spamMails),
+	)
+
+	// classifier.ConvertTermsFreqToTfIdf()
+
+
+	// dir := "dataset/test_data"
+	// emailList, err := ioutil.ReadDir(dir)
+	// if err != nil {
+	// 	fmt.Printf("Directory not present %s", err)
+	// }
+
+	// // Collecting all words from those emails
+	// for _, email := range emailList {
+	// 	dat, _ := ReadEmail(fmt.Sprintf("%s/%s", dir, email.Name()))
+	
+	// // Classify tokens from a document
+	// allScores, class, certain := classifier.Classify(dat)
+	// fmt.Println(allScores, class, certain)
+	// }
+
+
+	data, err := ioutil.ReadFile("dataset/spam_test.txt")
+	if err != nil {
+		fmt.Printf("File not present %s", err)
+	}
+
+		// Breaks the email into lines.
+		dat := strings.Split(string(data), "\n")
+
+		success := 0.0
+		failed := 0.0
+		total := float64(len(dat)-1)
+		//Last line is blank
+		for _, line := range dat[0:len(dat)-1]{
+			// Remove labels from start
+			words := strings.Split(line[2:len(line)], " ")
+				// Classify tokens from a document
+				allScores, class, certain := classifier.Classify(words)
+				// fmt.Println(allScores, class, certain)
+				// fmt.Println(verifySpam(line[0], string(class)))
+				// Rate of success
+				isSpam := verifySpam(line[0], string(class))
+				if (line[0] == 48 && !isSpam) || (line[0] == 49 && isSpam) {
+					success++
+				} else if !isSpam{
+					fmt.Println(allScores, class, certain)
+					fmt.Println(string(line[0]))
+					failed++
+				}
+		}
+		successPercent := (success/total)*100
+		failedPercent := (failed/total)*100
+
+		fmt.Println(successPercent)
+		fmt.Println(failedPercent)
+
+}
+
+func verifySpam(label byte, class string) bool{
+	if label == 49 && class == "spam" {
+		return true
+	} else if label == 48 && class == "fam" {
+		return false
+	}
+	// false if no case matched
+	return false
 }
